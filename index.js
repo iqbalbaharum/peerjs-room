e8781c729feb75303299c2e5e6bc7675b0ad57c8
iqbal <balainkk@gmail.com>
2022-06-19 20:22:40 +0800

    initial

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const app = express()
+const httpServer = require('http').createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+})
 
-app.listen(3000, () => {
-  console.log(`PeerJS Starting at port ${port}`)
+const { PeerServer } = require('peer')
+
+// start peerjs server
+const peerServer = PeerServer({
+  port: 9000,
+  path: '/',
+  proxied:  true,
+  allow_discovery: true,
+  config: { 'iceServers': [
+    { 'url': 'stun:stun.l.google.com:19302' }  
+  ]}
+})
+
+// on connection
+io.on('connection', (socket) => {
+  socket.on('join-room', (data) => {
+    socket.join(data.roomId)
+    socket.broadcast.emit('user-join', data.userId)
+  })
+
+  socket.on('disconnect', (data) => {
+    socket.broadcast.emit('user-leave', data.userId)
+  })
+})
+
+httpServer.listen(3010, () => {
+  console.log(`room socket.io starting at port ${3010}`)
+  console.log(`PeerJS server starting at port ${9000}`)
 })
